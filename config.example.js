daafe4f455982168fd0df37468d5064c4c0e71e6
ironarbor <sc@ironarborllc.com>
2026-04-08 15:35:30 -0400

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,5 @@
+window.IAPETS_CONFIG = {
+  SUPABASE_URL: "https://YOUR_PROJECT.supabase.co",
+  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY",
+  EDGE_BASE_URL: "https://YOUR_PROJECT.functions.supabase.co"
+};
